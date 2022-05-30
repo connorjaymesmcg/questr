193436f1a06614a07877f9376255a09002b695e5
Connor McGinnis <connorjaymesmcg@gmail.com>
2022-05-30 13:44:16 -0400

    basic UI applied to splash page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import sword from './images/sword.png';
+import useStyles from './styles';
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="staic" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Questbook
+        </Typography>
+        <img className={classes.image} src={sword} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
